6c854ede010ee7e771f91f03d56b9c126d598d39
Spycro <lucaszim16@gmail.com>
2020-01-15 22:27:19 +0100

    corr

--- src/Reader.h
+++ src/Reader.h
@@ -63,7 +63,7 @@ public:
     // Contrat :
     //
 
-    Reader (std::string logFile = "anonyme.log"));
+    Reader (std::string logFile = "anonyme.log");
     // Mode d'emploi :
     //
     // Contrat :
