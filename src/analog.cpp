06b097197c6b5cca50a8c954c50793c3ad4c351b
Spycro <lucaszim16@gmail.com>
2020-01-18 13:40:55 +0100

    edge case : "/"

--- src/analog.cpp
+++ src/analog.cpp
@@ -145,11 +145,11 @@ void parseData(rawData & data, bool exclude, bool date, string & heure, bool gra
     transform(data.referer.begin(), data.referer.end(), data.referer.begin(),::tolower);
     transform(data.target.begin(), data.target.end(), data.target.begin(),::tolower);
 
-    if(data.referer.back() == '/'){
+    if(data.referer.length() > 1 && data.referer.back() == '/'){
         data.referer.erase(data.referer.length() - 2, data.referer.length() - 1);
     }
 
-    if(data.target.back() == '/'){
+    if(data.target.length() > 1 data.target.back() == '/'){
         data.target.erase(data.target.length() - 2, data.target.length() - 1);
     }
 
